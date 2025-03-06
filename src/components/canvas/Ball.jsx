3e1ef46fb2395424aa28a5d9377d69b400d83708
DhruvPatel307 <dhruvp30072004@gmail.com>
2025-03-06 18:15:22 +0530

    commit

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,62 +1,4 @@
-// import React, { Suspense } from "react";
-// import { Canvas } from "@react-three/fiber";
-// import {
-//   Decal,
-//   Float,
-//   OrbitControls,
-//   Preload,
-//   useTexture,
-// } from "@react-three/drei";
-
-// import CanvasLoader from "../Loader";
-
-// const Ball = (props) => {
-//   const [decal] = useTexture([props.imgUrl]);
-
-//   return (
-//     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-//       <ambientLight intensity={0.25} />
-//       <directionalLight position={[0, 0, 0.05]} />
-//       <mesh castShadow receiveShadow scale={2.75}>
-//         <icosahedronGeometry args={[1, 1]} />
-//         <meshStandardMaterial
-//           color='#fff8eb'
-//           polygonOffset
-//           polygonOffsetFactor={-5}
-//           flatShading
-//         />
-//         <Decal
-//           position={[0, 0, 1]}
-//           rotation={[2 * Math.PI, 0, 6.25]}
-//           scale={1}
-//           map={decal}
-//           flatShading
-//         />
-//       </mesh>
-//     </Float>
-//   );
-// };
-
-// const BallCanvas = ({ icon }) => {
-//   return (
-//     <Canvas
-//       frameloop='demand'
-//       dpr={[1, 2]}
-//       gl={{ preserveDrawingBuffer: true }}
-//     >
-//       <Suspense fallback={<CanvasLoader />}>
-//         <OrbitControls enableZoom={false} />
-//         <Ball imgUrl={icon} />
-//       </Suspense>
-
-//       <Preload all />
-//     </Canvas>
-//   );
-// };
-
-// export default BallCanvas;
-
-import React, { Suspense, useEffect, useState, useMemo } from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
   Decal,
@@ -68,14 +10,14 @@ import {
 
 import CanvasLoader from "../Loader";
 
-const Ball = ({ imgUrl, isMobile }) => {
-  const [decal] = useMemo(() => useTexture([imgUrl]), [imgUrl]);
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={1.5} rotationIntensity={isMobile ? 0.5 : 1} floatIntensity={isMobile ? 1.5 : 2}>
-      <ambientLight intensity={isMobile ? 0.2 : 0.25} />
-      <directionalLight position={[0, 0, 0.05]} intensity={isMobile ? 0.5 : 1} />
-      <mesh castShadow receiveShadow scale={isMobile ? 2.0 : 2.75}>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
           color='#fff8eb'
@@ -96,26 +38,17 @@ const Ball = ({ imgUrl, isMobile }) => {
 };
 
 const BallCanvas = ({ icon }) => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 600px)");
-    setIsMobile(mediaQuery.matches);
-
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    };
-
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
-    return () => mediaQuery.removeEventListener("change", handleMediaQueryChange);
-  }, []);
-
   return (
-    <Canvas frameloop='demand' dpr={[1, 2]} gl={{ preserveDrawingBuffer: true }}>
+    <Canvas
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} isMobile={isMobile} />
+        <Ball imgUrl={icon} />
       </Suspense>
+
       <Preload all />
     </Canvas>
   );
